d273fa292c3f1fe565cbed36e7473bf148850e27
Deonetwo <48423286+deonetwo@users.noreply.github.com>
2019-05-10 09:21:47 +0700

    goyangnya asik

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -24,6 +24,7 @@ public class Hello {
     private printZefan(){
         System.out.println("Zefan grac almost no limit");
         System.out.println("Zefan grac almost no limit");
+        System.out.println("Zefan grac almost no limit");
         printZefan();
     }
     
